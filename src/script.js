31fa52f8fcac0579b126fdf1e27207a33503ad31
Martinasoomro <martinasoomro@hotmail.com>
2020-05-20 16:22:46 +0200

    added more javascript

--- src/script.js
+++ src/script.js
@@ -40,4 +40,46 @@ $(".left").click(function () {
 $(".right").click(function () {
   $(".news__slider").slick("slickNext");
 });
-// /*---------------------------------------------'/
+
+// /*-------------------FORM VALIDTION--------------------------'/
+
+const email = document.getElementById('email');
+    const name = document.getElementById('name');
+    const submit = document.getElementById('submit');
+    const submittedBox = document.getElementById('submitted-box');
+
+    const validateName = () => {
+        if (name.value === '') {
+            name.setCustomValidity('Please fill in your name');
+            return false;
+        } else {
+            name.setCustomValidity('');
+            return true;
+        }
+    }
+
+    const validateEmail = () => {
+        if (email.value === '' || email.validity.typeMismatch) {
+            email.setCustomValidity('Please fill in a valid email adress');
+            return false;
+        } else {
+        email.setCustomValidity('');
+        return true;
+        }
+    }
+
+    name.addEventListener('input', function () {
+        validateName();
+    });
+
+    email.addEventListener('input', function() {
+        validateEmail();
+    });
+
+    submit.addEventListener('click', function (event) {
+        if (validateName() && validateEmail()) {
+            submittedBox.style.display = 'block';
+            event.preventDefault();
+        }
+    });
+});
