3cdb3b482945a6dabf734c19cca9e8bbaadfa86c
Martinasoomro <martinasoomro@hotmail.com>
2020-05-20 16:06:18 +0200

    almost finished with examiniation 2

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// /*----------------SMOOTH SCROLL--------------*/
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
@@ -7,3 +9,35 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// /*---------------------HEADER SLIDER------------------*/
+
+$(".header-slider-container").slick({
+  infinite: true,
+  // autoplay: true,
+  autoplaySpeed: 4000,
+  dots: true,
+  dotsClass: "vertical-dots",
+  arrows: false,
+});
+
+// /*------------------------NEWS SLIDER--------------------*/
+
+$(".news-slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  dots: true,
+  arrows: false,
+});
+
+$(".left").click(function () {
+  $(".news__slider").slick("slickPrev");
+});
+
+$(".right").click(function () {
+  $(".news__slider").slick("slickNext");
+});
+// /*---------------------------------------------'/
